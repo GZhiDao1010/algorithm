229c2c005046044e5ec91179106527efe76b71b4
lixiaoqiang <gzhidao1010@gmail.com>
2020-05-12 09:00:59 +0800

    add

--- questions-easy/两个数组的交集 II.js
+++ questions-easy/两个数组的交集 II.js
@@ -1,4 +1,4 @@
-/*示例 1:
+/*示例 1: 两个数组的交集 II
 输入: nums1 = [1,2,2,1], nums2 = [2,2]
 输出: [2,2] */
 
@@ -6,6 +6,16 @@ const nums1 = [1,2,3,4,5]
 const nums2 = [4,5,6]
 
 // 哈希表
+/**时间复杂度O(n)
+
+先用Hashmap记录第一个数组中的元素【放在key】，和出现的次数【放在value】。
+
+然后再遍历第二个数组，如果找到对应元素，则添加这个元素到返回数组里。
+
+如果value值大于1，HashMap中的value值减 1，表示已经找到一个相同的了。
+
+如果value值等于1，则删除该元素。
+*/
 const intersect = (nums1,nums2)=>{
     let hash = new Map()
     let res = []
@@ -31,3 +41,8 @@ const intersect = (nums1,nums2)=>{
     return res
 }
 console.log('intersect(nums1,nums2) :>> ', intersect(nums1,nums2));
+
+const t1  = new Map([[2,'tet',{id:3}]])
+const t2 = new Map([[2,'dddd',{id:4}]])
+const merged = new Map([...t1,...t2])
+console.log('merged :>> ', merged);
